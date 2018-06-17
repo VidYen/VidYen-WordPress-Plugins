8e25607299d4ee8c73af892aa846c152ea7aa5b6
Felty <felty@vidyen.com>
2018-06-17 13:37:10 -0400

    Commented code removal
    Cleaning up for WordPress.org release. So far the commented code hasn't caused problems in the last few days of testing so removing it permantly.
    
    I got backups anyways.

--- VYPS_base/vyps.php
+++ VYPS_base/vyps.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: VidYen Point System Base Plugin
   Description: VidYen Point System (VYPS) allows you to gamify monetization by giving your users a reason to turn off adblockers for rewards.
-  Version: 0.0.33
+  Version: 0.0.34
   Author: VidYen, LLC
   Author URI: https://vidyen.com/
   License: GPLv2 or later
@@ -216,93 +216,6 @@ add_action('manage_users_custom_column', 'register_custom_user_column_view', 10,
 
 //end of add column in user table
 
-/* I am removing below but leaving in for now. May should remove for WP.org version
-*  but it needs to go as no longer needed as may interfere with BuddyPress
-*/
-
-/*
-function custom_user_profile_fields_points($user) {
-    global $wpdb;
-    $query = "select * from " . $wpdb->prefix . 'vyps_points';
-    $data = $wpdb->get_results($query);
-    ?>
-    <?php
-    if (!$_GET['user_id']) {
-        ?>
-        <!--<h3>Points</h3>-->
-    <?php } ?>
-    <table class="form-table">
-        <?php
-        if (!$_GET['user_id']) {
-            ?>
-                                                        <!--<tr>
-                                                            <th><label for="points">Points</label></th>
-                                                            <td>
-            <?php
-            $query_row = "select * from {$wpdb->prefix}vyps_points_log where user_id= '{$_GET['user_id']}'";
-            $row_data = $wpdb->get_row($query_row);
-
-            $user_points = get_the_author_meta('points', $user->ID);
-
-            if ($user_points) {
-                ?>
-                                                                                                                <input type="hidden" name="updateusers" value="<?php echo $user->ID ?>"/>
-                <?php
-            }
-            ?>
-                                                                <select class="points" id="points" name="points">
-                                                                    <option value=''>Select Points</option>
-            <?php if (!empty($data)): ?>
-                <?php foreach ($data as $d): ?>
-                                                                                                                                                                    <option <?php echo ($row_data->points == (string) $d->id) ? 'selected' : ''; ?> value='<?= $d->id ?>'><?= $d->name; ?></option>
-                <?php endforeach; ?>
-            <?php endif; ?>
-                                                                </select>                
-                                                                <span class="description">Assigned suitable point type</span>
-                                                            </td>
-                                                        </tr>-->
-        <?php } ?>
-        <tr>
-            <th><label for="vyps_age">Age</label></th>
-            <td>
-                <?php $vyps_age = get_the_author_meta('vyps_age', $user->ID); ?>
-                <input type="text" id="vyps_age" name="vyps_age" value="<?= $vyps_age ?>">
-            </td>
-        </tr>
-        <tr>
-            <th><label for="vyps_sex">Sex</label></th>
-            <td>
-                <?php $vyps_sex = get_the_author_meta('vyps_sex', $user->ID); ?>
-                <select id="vyps_sex" name="vyps_sex">
-                    <option value="Male" <?= ($vyps_sex == "Male") ? 'selected' : ''; ?>>Male</option>
-                    <option value="Female" <?= ($vyps_sex == "Female") ? 'selected' : ''; ?>>Female</option>
-                </select>                
-            </td>
-        </tr>
-        <tr>
-            <th><label for="vyps_country">Country</label></th>
-            <td>
-                <?php $vyps_country = get_the_author_meta('vyps_country', $user->ID); ?>
-                <input type="text" id="vyps_country" name="vyps_country" value="<?= $vyps_country ?>">
-            </td>
-        </tr>
-        <tr>
-            <th><label for="vyps_org">Organization</label></th>
-            <td>
-                <?php $vyps_org = get_the_author_meta('vyps_org', $user->ID); ?>
-                <input type="text" id="vyps_org" name="vyps_org" value="<?= $vyps_org ?>">
-            </td>
-        </tr>
-    </table>
-    <?php
-}
-
-*/
-
-/* I am going to try removing the below and see if it breaks anything. Revert if needed */
-
-
-
 if (isset($_POST['updateusers'])) {
 
 
@@ -325,25 +238,7 @@ if (isset($_POST['updateusers'])) {
 	   // again do this only if you can
         if (!current_user_can('manage_options'))
             return false;
-		
-		/* I'm going to comment this whole thing as I realize VYPS should not longer mess with the profiles * /
-		/*
-        # save my custom field
-        update_user_meta($user_id, 'points', $_POST['points']);
-        update_user_meta($user_id, 'vyps_age', $_POST['vyps_age']);
-        update_user_meta($user_id, 'vyps_sex', $_POST['vyps_sex']);
-        update_user_meta($user_id, 'vyps_country', $_POST['vyps_country']);
-        update_user_meta($user_id, 'vyps_org', $_POST['vyps_org']);
-        global $wpdb;
-        $table = $wpdb->prefix . 'vyps_points_log';
-        $data = [
-            'points' => $_POST['points'],
-            'user_id' => $user_id,
-            'time' => date('Y-m-d H:i:s')
-        ];
-        $wpdb->insert($table, $data);
-		
-		*/
+
     }
 
     add_action('user_register', 'save_custom_user_profile_fields_points');
