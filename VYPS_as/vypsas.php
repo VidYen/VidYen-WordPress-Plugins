8ad3340b9b56d7f18d5835227667c90293bbdd21
Felty <felty@vidyen.com>
2018-06-21 15:43:53 -0400

    Adscend update for btn
    I thought about showing how many points you need to redeem, but realized it was redudant.

--- VYPS_as/vypsas.php
+++ VYPS_as/vypsas.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: VYPS Adscend Media API Addon
   Description: Earn VYPS points by doing AdScend Media activities
-  Version: 0.0.19
+  Version: 0.0.21
   Author: VidYen, LLC
   Author URI: https://vidyen.com/
   License: GPLv2 or later
@@ -111,6 +111,8 @@ function vyps_as_sub_menu_page()
 	<p>To have a user redeem points through the Adscend API (the points Adscend has said they earned). You need to get your own API off your Adscend wall page. The API key is on the integration page on your offer wall under API/SDK integration.</p>
 	<p><b>[vyps-as-redeem pub=113812 profile=13246 api=typekeyhere pid=1 payout=750]</b></p>
 	<p>All attributes must be set for this to function. There is no interface and is up to the site admin to add shortcode to a page or button. Future versions will include a better interface.</p>
+	<p><b>[vyps-as-redeem-btn pub=113812 profile=13246 api=typekeyhere pid=1 payout=750]</b></p>
+	<p>Using the btn shortcode will just have a redemption function that calls the function without having to create seperate pages.</p>
 	";
 	
 	//Credits include
@@ -387,5 +389,208 @@ function as_redeem_func( $atts ) {
 		
 }
 
-add_shortcode( 'vyps-as-redeem', 'as_redeem_func');	
+add_shortcode( 'vyps-as-redeem', 'as_redeem_func');
+
+/*** OK. Adding redeem with button. Copy and paste of above function in case admin did not want GUI ***/
+
+function as_redeem_btn_func( $atts ) {
+	
+	/* Do the logged on check first as I guess it wastes less resources */
+	
+	if ( is_user_logged_in() ) {
+		
+		//I probaly don't have to have this part of the if
+		
+	} else {
+		
+		return "You need to be logged in to watch ads for points.";
+		
+	}
+	
+	global $wpdb;
+	$table_name_log = $wpdb->prefix . 'vyps_points_log';
+	$table_name_as = $wpdb->prefix . 'vyps_as';
+	$current_user_id = get_current_user_id();
+	
+	$atts = shortcode_atts(
+		array(
+			'pub' => '0',
+			'profile' => '0',
+			'api' => 'z',
+			'pid' => '0',
+			'payout' => '0',
+		), $atts, 'vyps-as-redeem');
+	
+	/* do the normal checks to see if the $atts were set */
+	
+	if ( $atts['pub'] == 0 ) {
+		
+	return "Publisher was not set!";
+		
+	}
+	
+	/* Oh yeah. Check Profile */
+	
+	if ( $atts['profile'] == 0 ) {
+		
+		return "You did not a profile!";
+		
+	}
+	
+	/* Oh yeah. Checking to see if no API was set
+	*  Yeah I didn't like not putting APIs in shortcode
+	*  But Adscend was being a pain. Oh. The API key is on
+	*  the integration page on your offer wall under API/SDK
+	*  integration. It doesn't even look like its a menu.
+	*  It's like that scend in HHG2G trying to get to the form.
+	*  It's a shame Adscend didn't copy Coin Hive.
+	*/
+	
+	//return $atts['api']; //return here to see why api key was not working
+	
+	
+	/* API key will never be a single character in theory but I needed something easy to check #lazycoding */
+	if ( $atts['api'] == 'z' ) { 
+		
+		return "You did not set the API Key!";
+		
+	}
+	
+	/* Oh yeah. Checking to see if no pid was set */
+	
+	if ( $atts['pid'] == 0 ) {
+		
+		return "You did not set point ID!";
+		
+	}
+	
+	//In theory one could set their payout to be 0 on purpose, but if you are that kind of person just comment this if out
+	
+	if ( $atts['payout'] == 0 ) {
+		
+		return "You did not set payout!";
+		
+	}
+	
+	/* Ok. This might be lazy coding and Grey Goose but I figured we can just see if the button has been clicked */
+	
+	if (isset($_POST["redeem"])){ 
+	
+	} else {
+		
+		/* Just show them button if button has not been clicked. Its a requirement not a suggestion. */
+		return "<form method=\"post\">
+                <input type=\"hidden\" value=\"\" name=\"redeem\"/>
+                <input type=\"submit\" class=\"button-secondary\" value=\"Reedem Adscend\" onclick=\"return confirm('You are about to synce your Adscend point with this site. Are you sure?');\" />
+                </form>";		
+	}
+
+	/* I have a feeling I could check the whole array in one go, but one day I will educate myself better */
+	
+	/* Ok now we need to post the current leads to the as table and then check to see if there is more than
+	*  one row with that user id and then if so caclulate the difference and post that reward to the vyps log
+	*  the default leads will be zero so if there wasn't a row to begin with then all the points get awarded
+	*/
+	
+	/* Hrm... The below does generate the correct json but its not pulling for some reason soo... I'm going to use the CH version */
+	/* It dawned on me that the ' ' in arrays might be the problem but below is copy and paste from coin hive*/
+	
+	$pub_id = $atts['pub'];
+	$adwall_id = $atts['profile'];
+	//$sub_id = 4; //I don't running those ads on my development machine
+	$sub_id = $current_user_id; //ok the testing words so lets use another profile
+	
+	/* The get curl */
+	
+	$url = "https://adscendmedia.com/adwall/api/publisher/{$pub_id}/profile/{$adwall_id}/user/{$sub_id}/transactions.json";
+	
+	//Note Api says no https but well I feel it should be so and it seems to work					
+	
+	$as = curl_init();
+	curl_setopt($as, CURLOPT_URL, $url);
+	curl_setopt($as, CURLOPT_HEADER, 0);
+	curl_setopt($as, CURLOPT_RETURNTRANSFER, true);
+	$result = curl_exec($as);
+	curl_close($as);
+										
+	$jsonData = json_decode($result, true);
+	$balance = $jsonData['currency_count'];
+	
+	//echo " pre foo ";
+	//echo $balance;
+	//echo " post foo ";
+	
+	/* the post to deduct the currency */
+	
+	//
+	// A very simple PHP example that sends a HTTP POST to a remote site
+	//
+	
+	$api_key = $atts['api'];
+	
+	$adj_balance = $balance * -1; //Well. Apparently you can give your viewers more points for no good reason I guess. So we need negative values.
+	
+	$url = "https://adscendmedia.com/adwall/api/publisher/{$pub_id}/profile/{$adwall_id}/user/{$sub_id}/transactions.json";
+	//$url = "https://adscendmedia.com/adwall/api/publisher/{$pub_id}/profile/{$adwall_id}/user/{$sub_id}/transactions.json?api_key={$api_key}&currency_adjustment={$adj_balance}";
+	
+	
+	$as = curl_init();									
+	curl_setopt($as, CURLOPT_URL, $url);
+	curl_setopt($as, CURLOPT_POST, 1);
+	curl_setopt($as, CURLOPT_POSTFIELDS,
+		"api_key={$api_key}&currency_adjustment={$adj_balance}");
+										
+	// in real life you should use something like:
+	// curl_setopt($ch, CURLOPT_POSTFIELDS, 
+	//          http_build_query(array('postvar1' => 'value1')));
+								
+	// receive server response ...
+	curl_setopt($as, CURLOPT_RETURNTRANSFER, true);
+									
+	$server_output = curl_exec ($as);
+										
+	curl_close ($as);
+	
+	/* OK. Pulling log table to post return to it. What could go wrong? */
+	/* Honestly, we should always refer to table by the actual table?   */
+		
+	/* Just checking to see if balance is 0. If it is, no need to do anything other than return the results.*/
+	if( $balance > 0 )
+		{
+			global $wpdb;
+			
+			$table_log = $wpdb->prefix . 'vyps_points_log';
+			$reason = "Adscend";
+			$amount = $balance * $atts['payout']; //
+
+			//$pointType = $wpdb->get_var( "SELECT * FROM $table_ch", 6, 0 ); //it dawned on me I could make the CH not have its own table maybe for 2.0
+			$pointType = $atts['pid']; //
+			$user_id = get_current_user_id();
+				$data = [
+					'reason' => $reason,
+					'points' => $pointType,
+					'points_amount' => $amount,
+					'user_id' => $user_id,
+					'time' => date('Y-m-d H:i:s')
+				];
+			$wpdb->insert($table_log, $data);
+		} else {
+			$amount = 0; //I think this works right. Ere we go!
+		}
+		
+		/* It dawned on me that text in here only needs a number and let the admin right there response
+		*  One could in theory might make an IF statement you have no hashes to redeem, but KISS */
+	//echo " end balance comes after ";	
+	
+	/* Developers note. I try to put working into site admins mouth but this only way to show button and the redeemed points in a way that I liked.
+	*  In theory if admin doesn't like it, they edit the below wording or just use the old shortcodes which I left in. -Felty
+	*/
+	return $amount . " points redeemed. <br><br><form method=\"post\">
+                <input type=\"hidden\" value=\"\" name=\"redeem\"/>
+                <input type=\"submit\" class=\"button-secondary\" value=\"Reedem Adscend\" onclick=\"return confirm('You are about to synce your Adscend point with this site. Are you sure?');\" />
+                </form>";		
+		
+}
+
+add_shortcode( 'vyps-as-redeem-btn', 'as_redeem_btn_func');
 
