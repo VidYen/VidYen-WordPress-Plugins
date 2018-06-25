102c361f0667587fb75ca9b393b4ce8dd88e0f52
Felty <felty@vidyen.com>
2018-06-24 23:54:27 -0400

    WW integration
    not finished but it worked while testing

--- VYPS/includes/credits.php
+++ VYPS/includes/credits.php
@@ -6,16 +6,13 @@
  echo "<br><br>
 	<h2>Here is a list of our other addons that go along with this system:</h2>
 	<p>VYPS AdScend Addon</p>
-	<p>VYPS Balance Shortcode Addon</p>
 	<p>VYPS CoinFlip Addon</p>
 	<p>VYPS Coinhive Addon</p>
-	<p>VYPS Point Transfer Addon</p>
-	<p>VYPS Public Log Shortcode Addon</p>
 	<p>VYPS WooWallet Addon</p>
 	<br><br>
 	<h2>Coming Soon</h2>
 	<p>VYPS Combat Game</p>
-	<p>Leader Board Addon</p>
+	<p>Leaderboard Addon</p>
 	";
 
 /* You know I'm not entirely sure if the ending of this PHP will cause problems.
--- VYPS/includes/sc_instruct.php
+++ VYPS/includes/sc_instruct.php
@@ -17,7 +17,7 @@
 	<p>The pid is the pointID number seen on the points list page along with the uid which is the user id in WordPress. Leaving the uid option out (ie. [vyps-balance-list pid=1]  will default to the logged on user.</p>
 	<p>Note: Leaving the uid blank will tell the user they need to log in if you intend to show this to users who are not log in.</p>
 	<p>Also Note: pid will default to 1 which is the first point you have unless you delete it. I would recommend specifing pid at all times.</p>
-	<h2>VYPS Point Transfer</h2>
+	<h2>Point Transfer</h2>
 	<p>This plugin needs VYPS Base and two point types to function. The intention is to allow a quick and easy way for users to transfer one type of point to another at varrying rates</p>
 	<p><b>[vyps-pt spid=1 dpid=2 samount=100000 damount=100]</b></p>
 	<p>Function debits points from one point type to another with in being how many points used to transfer and out as how many points they get in the new point type</p>
@@ -26,6 +26,8 @@
 	<p>All attributes must be set for this to function. There is no interfact and is up to the site admin to add shortcode to a page or button.</p>
 	<p><b>[vyps-pt-btn spid=1 dpid=2 samount=100000 damount=100]</b></p>
 	<p>Does the same except now has built in button and user confirmation rather than a direct page call.</p>
+	<p><b>[vyps-pt-tbl spid=1 dpid=2 samount=100000 damount=100]</b></p>
+	<p>Creates a table interface UI for the point transfer which includes a button and better interface including point icons and feedback. If you do not want to mess with your special layout, use this.</p>
 	";
 
 /* You know I'm not entirely sure if the ending of this PHP will cause problems.
