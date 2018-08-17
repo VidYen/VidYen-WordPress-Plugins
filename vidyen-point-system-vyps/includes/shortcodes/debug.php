5f4af24c9f014e0a0255cbef538ab260dc05776c
Felty <felty@vidyen.com>
2018-08-17 18:48:24 -0400

    curl update

--- vidyen-point-system-vyps/includes/shortcodes/debug.php
+++ vidyen-point-system-vyps/includes/shortcodes/debug.php
@@ -9,12 +9,31 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 //$remote_url = "http://vy256.com:8081/?userid=" . $miner_id;
 
-function vyps_debug_func(){
+function vyps_debug_wp_remote_func(){
 
   //wp_remote_get
   $remote_url = "http://vy256.com:8081/?userid=worker_11_48Vi6kadiTtTyemhzigSDrZDKcH6trUTA7zXzwamziSmAKWYyBpacMjWbwaVe4vUMveKAzAiA4j8xgUi29TpKXpm3wL5K5a_vidyenlive355";
   $remote_response =  wp_remote_get( esc_url_raw( $remote_url ) );
-  $balance =  intval($remote_response['body']);
+  $vyps_balance =  intval($remote_response['body']);
+
+  //Google get
+  $remote_url = "http://www.google.com";
+  $remote_response =  wp_remote_get( esc_url_raw( $remote_url ) );
+  $google_balance =  intval($remote_response['body']);
+
+  $remote_remote_debug = "wp_remote_get test to VY256:<br>
+    $vyps_balance<br>
+    <br>
+    wp_remote_get test to Google:<br>
+    $google_balance<br><br>";
+
+  return $remote_remote_debug;
+
+}
+
+add_shortcode( 'vyps-debug-get', 'vyps_debug_wp_remote_func');
+
+function vyps_curl_debug(){
 
   //curl
   //$url = "https://api.coinhive.com/user/balance?name={$hiveUser}&secret={$hiveKey}";
@@ -29,22 +48,36 @@ function vyps_debug_func(){
 
   //$jsonData = json_decode($result, true);
   //$balance = $jsonData['balance'];
-  $curl_result = $result;
+  $vyps_curl_result = $result;
+
+  //curl for googtle
+  //$url = "https://api.coinhive.com/user/balance?name={$hiveUser}&secret={$hiveKey}";
+  $url = "http://www.google.com";
+
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $result = curl_exec($ch);
+  curl_close($ch);
+
+  //$jsonData = json_decode($result, true);
+  //$balance = $jsonData['balance'];
+  $google_curl_result = $result;
 
 
-  $remote_debug = "wp_remote_get test to VY256:<br>
-    $balance<br>
-    <br>
-    curl test to VY256:<br>
-    $result";
 
-  return $remote_debug;
+  return "VY256 curl return:<br>
+  $vyps_curl_result<br><br>
+  Googel curl return<br>
+  $google_curl_result<br><br>
+  ";
 
 }
 
-add_shortcode( 'vyps-debug', 'vyps_debug_func');
+add_shortcode( 'vyps-debug-curl', 'vyps_curl_debug');
 
-function vyps_hw_func (){
+function vyps_hw_func(){
 
   return "Hello world";
 
--- vidyen-point-system-vyps/vidyen-point-system-vyps.php
+++ vidyen-point-system-vyps/vidyen-point-system-vyps.php
@@ -4,7 +4,7 @@
 Plugin Name:  VidYen Point System [VYPS]
 Plugin URI:   http://vyps.org
 Description:  VidYen Point System [VYPS] allows you to create a rewards site using video ads or browser mining.
-Version:      00.04.09.09
+Version:      00.04.09.12
 Author:       VidYen, LLC
 Author URI:   https://vidyen.com/
 License:      GPLv2
