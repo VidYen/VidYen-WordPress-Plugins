6f2812b043bf7c7d856d5667e04df472838e191b
Felty <40067341+VidYen@users.noreply.github.com>
2018-12-10 20:11:38 -0500

    update nonce and odds

--- vidyen-point-system-vyps/includes/shortcodes/vyps-quads.php
+++ vidyen-point-system-vyps/includes/shortcodes/vyps-quads.php
@@ -27,6 +27,9 @@ function vyps_rng_quads_func( $atts )
       return;
   }
 
+  //Adding a nonce to the post
+  $vyps_nonce_check = wp_create_nonce( 'vyps-nonce-quads' );
+
   //Get the url for the Quads js
   $vyps_quads_jquery_folder_url = plugins_url( 'js/jquery/', __FILE__ );
   $vyps_quads_jquery_folder_url = str_replace('shortcodes/', '', $vyps_quads_jquery_folder_url); //having to reomove the folder depending on where you plugins might happen to be
@@ -49,31 +52,23 @@ function vyps_rng_quads_func( $atts )
         }
         document.getElementById(\"animated_number_output\").style.display = 'block'; // disable button
         document.getElementById(\"number_output\").style.display = 'none'; // enable button
+        document.getElementById(\"results_div\").style.display = 'none'; // enable button
         jQuery(document).ready(function($) {
          var data = {
            'action': 'vyps_run_quads_action',
            'multicheck': multi,
+           'vypsnoncepost': '$vyps_nonce_check',
          };
          // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
          jQuery.post(ajaxurl, data, function(response) {
            output_response = JSON.parse(response);
            document.getElementById('number_output').innerHTML = output_response.full_numbers;
            document.getElementById('current_balance').innerHTML = output_response.post_balance;
-            document.getElementById('reward_balance').innerHTML = output_response.response_text  + ': ';
+           document.getElementById('response_text').innerHTML = output_response.response_text  + ' - Earned: ';
            document.getElementById('reward_balance').innerHTML = output_response.reward;
-           var elem = document.getElementById(\"texta\");
-           elem.value += \"Date:[\" + new Date().toLocaleString() + \"]\";
-           elem.value += \"\\n\";
-           elem.value += \"Post Balance:[\" + output_response.post_balance + \"] \";
-           elem.value += \"\\n\";
-           elem.value += \"Reward:[\" + output_response.reward + \"] \";
-           elem.value += \"\\n\";
-           elem.value += output_response.response_text;
-           elem.value += \"\\n\";
-           elem.scrollTop = elem.scrollHeight;
            document.getElementById(\"animated_number_output\").style.display = 'none'; // disable button
            document.getElementById(\"number_output\").style.display = 'block'; // enable button
-           //clearInterval(randomtime);
+           document.getElementById(\"results_div\").style.display = 'block'; // enable button
          });
         });
       }
@@ -83,15 +78,12 @@ function vyps_rng_quads_func( $atts )
       }
 
     </script>
-    <div align=\"center\"><span id=\"animated_number_output\" style=\"display:block\">0000</span></div>
-    <div align=\"center\"><spawn id=\"response_text\"></span><span>$icon_url</span><span id=\"number_output\" style=\"display:none\">0000</span></div>
+    <div align=\"center\"><span id=\"animated_number_output\" style=\"display:block; font-size:40px\">0000</span></div>
+    <div align=\"center\"><span id=\"number_output\" style=\"display:none; font-size:40px\">0000</span></div>
     <div id=\"bet_action1\" align=\"center\" style=\"width:100%;\"><button onclick=\"gettherng(1)\" style=\"width:50%;\">$icon_url 1</button><button onclick=\"gettherng(10)\" style=\"width:50%;\">$icon_url 10</button></div>
     <div id=\"bet_action2\" align=\"center\" style=\"width:100%;\"><button onclick=\"gettherng(100)\" style=\"width:50%;\">$icon_url 100</button><button onclick=\"gettherng(1000)\" style=\"width:50%;\">$icon_url 1000</button></div>
-    <table>
-      <tr><div align=\"center\"><span id=\"current_balance\">$starting_balance_html</span></div></tr>
-      <tr><div align=\"center\"><span id=\"reward_balance\">$icon_url 0</span></div></tr>
-    <table>
-    <div align=\"center\"><textarea rows=\"4\" cols=\"50\" id=\"texta\"></textarea></div>
+    <div align=\"center\"><span>Balance: </span><span id=\"current_balance\">$starting_balance_html</span></div>
+    <div id=\"results_div\" align=\"center\" style=\"display:none;\"><span id=\"response_text\"></span><span>$icon_url </span><span id=\"reward_balance\">0</span></div>
     ";
 
     return $vyps_rng_quads_html_output;
@@ -112,11 +104,12 @@ function vyps_run_quads_action()
 
   global $wpdb; // this is how you get access to the database
 
+  //This should cause a die. If the nonce fails.
+  check_ajax_referer( 'vyps-nonce-quads', 'vypsnoncepost' );
+
   $incoming_multiplier = intval( $_POST['multicheck'] );
 
-  //$whatever += 10;
-
-  // add your logic here...
+  // Shortcode additions.
   $atts = shortcode_atts(
 		array(
 				'outputid' => 1,
@@ -190,49 +183,49 @@ function vyps_run_quads_action()
   {
     //WE got quads
     $response_text = "QUADS";
-    $reward_amount = $bet_cost * 1000;
+    $reward_amount = $bet_cost * 100;
     $rng_numbers_combined = '<b>' . $digit_first . $digit_second . $digit_third . $digit_fourth . '</b>'; //Bolding for end user
   }
   elseif (($digit_first == $digit_second) AND ($digit_first == $digit_third))
   {
     //We got trips on first 3
     $response_text = "TRIPS";
-    $reward_amount = $bet_cost * 100;
+    $reward_amount = $bet_cost * 10;
     $rng_numbers_combined = '<b>' . $digit_first . $digit_second . $digit_third . '</b>' . $digit_fourth; //First three bold
   }
   elseif (($digit_second == $digit_third) AND ($digit_second == $digit_fourth))
   {
     //trips on last 3
     $response_text = "TRIPS";
-    $reward_amount = $bet_cost * 100;
+    $reward_amount = $bet_cost * 10;
     $rng_numbers_combined = $digit_first . '<b>' . $digit_second . $digit_third . $digit_fourth . '</b>'; //Last three bold
   }
   elseif ($digit_first == $digit_second)
   {
     //dubs on first 2
     $response_text = "DUBS";
-    $reward_amount = $bet_cost * 10;
+    $reward_amount = $bet_cost * 5;
     $rng_numbers_combined = '<b>' . $digit_first . $digit_second . '</b>' . $digit_third . $digit_fourth; //First two
   }
   elseif ($digit_second == $digit_third)
   {
     //dubs on  middle 2
     $response_text = "DUBS";
-    $reward_amount = $bet_cost * 10;
+    $reward_amount = $bet_cost * 5;
     $rng_numbers_combined = $digit_first . '<b>' . $digit_second . $digit_third . '</b>' . $digit_fourth; //Middle two
   }
   elseif ($digit_third == $digit_fourth)
   {
     //dubs on last 2
     $response_text = "DUBS";
-    $reward_amount = $bet_cost * 10;
+    $reward_amount = $bet_cost * 5;
     $rng_numbers_combined = $digit_first . $digit_second . '<b>' . $digit_third . $digit_fourth . '</b>'; //Last two
   }
   elseif ($digit_first == $digit_second AND $digit_third == $digit_fourth )
   {
     //ddouble dubs
     $response_text = "DOUBLEDUBS";
-    $reward_amount = $bet_cost * 500;
+    $reward_amount = $bet_cost * 50;
     $rng_numbers_combined = $digit_first . $digit_second . '<b>' . $digit_third . $digit_fourth . '</b>'; //Last two
   }
   else
