e7b4aa61cd7af57ff5ea4645f58c28919ce81777
Felty <40067341+VidYen@users.noreply.github.com>
2019-01-02 10:39:54 -0500

    updates

--- vidyen-point-system-vyps/includes/functions/core/vyps_public_log_func.php
+++ vidyen-point-system-vyps/includes/functions/core/vyps_public_log_func.php
@@ -54,7 +54,6 @@ function vyps_public_log_func( $atts ) {
 	//BTW the number of IDs should always match the number of rows, NO EXCEPTIONS. If it doesn't it means the admin deleted a row
 	//And that is against the psuedo-blockchain philosophy. //Also it dawned on me I can rewrite the public log here.
 
-
 	//$number_of_log_rows = $wpdb->get_var( "SELECT max( id ) FROM $table_name_log" ); //No WHERE needed. All rows. No exceptions
   $number_of_log_rows_query = "SELECT max( id ) FROM ". $table_name_log;  //I'm wondering if a prepare is even needed, but throw it all in.
   $number_of_log_rows = $wpdb->get_var( $number_of_log_rows_query ); //Ok. I realized that not only prepare() doesn't work it, there is no varialbes needed to sanitize as the table name is actually hard coded.
@@ -204,84 +203,169 @@ function vyps_public_log_func( $atts ) {
 				$table_range_stop = 1; //If we go below 1, then just hard floor it at 1 as no 0 or negative transaction numbers exists.
 	}
 
-	//The number of log rows will always but correct but its the starting point and end points that will change.
-	for ($x_for_count = $table_range_start; $x_for_count >= $table_range_stop; $x_for_count = $x_for_count - 1 ) { //I'm counting backwards. Also look what I did. Also also, there should never be a 0 id or less than 1
+	//Setting a check if its not the current user check via short code, then we just run the following code as normal.
+	if ($current_user_state != TRUE)
+	{
+		//The number of log rows will always but correct but its the starting point and end points that will change.
+		for ($x_for_count = $table_range_start; $x_for_count >= $table_range_stop; $x_for_count = $x_for_count - 1 ) //I'm counting backwards. Also look what I did. Also also, there should never be a 0 id or less than 1
+		{
+			//$date_data = $wpdb->get_var( "SELECT time FROM $table_name_log WHERE id= '$x_for_count'" ); //Straight up going to brute force this un-programatically not via entire row
+			$date_data_query = "SELECT time FROM ". $table_name_log . " WHERE id = %d";
+			$date_data_query_prepared = $wpdb->prepare( $date_data_query, $x_for_count );
+			$date_data = $wpdb->get_var( $date_data_query_prepared );
 
-		//$date_data = $wpdb->get_var( "SELECT time FROM $table_name_log WHERE id= '$x_for_count'" ); //Straight up going to brute force this un-programatically not via entire row
-		$date_data_query = "SELECT time FROM ". $table_name_log . " WHERE id = %d";
-		$date_data_query_prepared = $wpdb->prepare( $date_data_query, $x_for_count );
-		$date_data = $wpdb->get_var( $date_data_query_prepared );
+			//$user_id_data = $wpdb->get_var( "SELECT user_id FROM $table_name_log WHERE id= '$x_for_count'" );
+			$user_id_data_query = "SELECT user_id FROM ". $table_name_log . " WHERE id = %d";
+			$user_id_data_query_prepared = $wpdb->prepare( $user_id_data_query, $x_for_count );
+			$user_id_data = $wpdb->get_var( $user_id_data_query_prepared );
+			$user_id_validated = intval($user_id_data); //I added this extra line to make the return an int as it wasn't being compared correctly as was coming out as a string not a number.
 
-		//$user_id_data = $wpdb->get_var( "SELECT user_id FROM $table_name_log WHERE id= '$x_for_count'" );
-		$user_id_data_query = "SELECT user_id FROM ". $table_name_log . " WHERE id = %d";
-		$user_id_data_query_prepared = $wpdb->prepare( $user_id_data_query, $x_for_count );
-		$user_id_data = $wpdb->get_var( $user_id_data_query_prepared );
-		$user_id_validated = intval($user_id_data); //I added this extra line to make the return an int as it wasn't being compared correctly as was coming out as a string not a number.
+			//$display_name_data = $wpdb->get_var( "SELECT display_name FROM $table_name_users WHERE id= '$user_id_data'" ); //And this is why I didn't call it the entire row by arrow. We are in 4d with multiple tables
+			$display_name_data_query = "SELECT display_name FROM ". $table_name_users . " WHERE id = %d"; //Note: Pulling from WP users table
+			$display_name_data_query_prepared = $wpdb->prepare( $display_name_data_query, $user_id_data );
+			$display_name_data = $wpdb->get_var( $display_name_data_query_prepared );
 
-		//$display_name_data = $wpdb->get_var( "SELECT display_name FROM $table_name_users WHERE id= '$user_id_data'" ); //And this is why I didn't call it the entire row by arrow. We are in 4d with multiple tables
-		$display_name_data_query = "SELECT display_name FROM ". $table_name_users . " WHERE id = %d"; //Note: Pulling from WP users table
-		$display_name_data_query_prepared = $wpdb->prepare( $display_name_data_query, $user_id_data );
-		$display_name_data = $wpdb->get_var( $display_name_data_query_prepared );
+			//$point_id_data = $wpdb->get_var( "SELECT point_id FROM $table_name_log WHERE id= '$x_for_count'" );
+			$point_id_data_query = "SELECT point_id FROM ". $table_name_log . " WHERE id = %d";
+			$point_id_data_query_prepared = $wpdb->prepare( $point_id_data_query, $x_for_count );
+			$point_id_data = $wpdb->get_var( $point_id_data_query_prepared );
 
-		//$point_id_data = $wpdb->get_var( "SELECT point_id FROM $table_name_log WHERE id= '$x_for_count'" );
-		$point_id_data_query = "SELECT point_id FROM ". $table_name_log . " WHERE id = %d";
-		$point_id_data_query_prepared = $wpdb->prepare( $point_id_data_query, $x_for_count );
-		$point_id_data = $wpdb->get_var( $point_id_data_query_prepared );
+			//$point_type_data = $wpdb->get_var( "SELECT name FROM $table_name_points WHERE id= '$point_id_data'" );
+			$point_type_data_query = "SELECT name FROM ". $table_name_points . " WHERE id = %d";
+			$point_type_data_query_prepared = $wpdb->prepare( $point_type_data_query, $point_id_data );
+			$point_type_data = $wpdb->get_var( $point_type_data_query_prepared );
 
-		//$point_type_data = $wpdb->get_var( "SELECT name FROM $table_name_points WHERE id= '$point_id_data'" );
-		$point_type_data_query = "SELECT name FROM ". $table_name_points . " WHERE id = %d";
-		$point_type_data_query_prepared = $wpdb->prepare( $point_type_data_query, $point_id_data );
-		$point_type_data = $wpdb->get_var( $point_type_data_query_prepared );
+			//$amount_data = $wpdb->get_var( "SELECT points_amount FROM $table_name_log WHERE id= '$x_for_count'" );
+	    $amount_data_query = "SELECT points_amount FROM ". $table_name_log . " WHERE id = %d";
+	    $amount_data_query_prepared = $wpdb->prepare( $amount_data_query, $x_for_count );
+	    $amount_data = $wpdb->get_var( $amount_data_query_prepared );
 
-		//$amount_data = $wpdb->get_var( "SELECT points_amount FROM $table_name_log WHERE id= '$x_for_count'" );
-    $amount_data_query = "SELECT points_amount FROM ". $table_name_log . " WHERE id = %d";
-    $amount_data_query_prepared = $wpdb->prepare( $amount_data_query, $x_for_count );
-    $amount_data = $wpdb->get_var( $amount_data_query_prepared );
+			//$reason_data = $wpdb->get_var( "SELECT reason FROM $table_name_log WHERE id= '$x_for_count'" );
+	    $reason_data_query = "SELECT reason FROM ". $table_name_log . " WHERE id = %d";
+	    $reason_data_query_prepared = $wpdb->prepare( $reason_data_query, $x_for_count );
+	    $reason_data = $wpdb->get_var( $reason_data_query_prepared );
 
-		//$reason_data = $wpdb->get_var( "SELECT reason FROM $table_name_log WHERE id= '$x_for_count'" );
-    $reason_data_query = "SELECT reason FROM ". $table_name_log . " WHERE id = %d";
-    $reason_data_query_prepared = $wpdb->prepare( $reason_data_query, $x_for_count );
-    $reason_data = $wpdb->get_var( $reason_data_query_prepared );
+			//If statement to pop in the UID if There
+			//Just popping in to the table if there. Hopefully it doesn't blow up the existing table
+			if( $uid_on == TRUE )
+			{
+				$uid_data_row = "<td>$user_id_data</td>";
+			}
+			else
+			{
+				$uid_data_row = "";
+			}
 
-		//If statement to pop in the UID if There
-		//Just popping in to the table if there. Hopefully it doesn't blow up the existing table
-		if( $uid_on == TRUE ){
+			$current_row_output = "
+				<tr>
+					<td>$x_for_count</td>
+					<td>$date_data</td>
+					$uid_data_row
+					<td>$display_name_data</td>
+					<td>$point_type_data</td>
+					<td>$amount_data</td>
+					<td>$reason_data</td>
+				</tr>
+					";
 
-			$uid_data_row = "<td>$user_id_data</td>";
-
-		}	else {
-
-			$uid_data_row = "";
-
+			//Code inserted to see if a user id was specified. If so, we are creating a table just for that user_id.
+			if( $user_id == 0)
+			{
+				//Compile into row output.
+				$table_output = $table_output . $current_row_output; //I like my way that is more reasonable instead of .=
+			}
+			elseif ( $user_id_validated == $user_id AND $user_id > 0)
+			{
+				//The idea above is to see if the query for the sql UID pull (validated) $user_id is the same as the query AND is greater than zero.
+				//In theory, you could put a negative number in, but not sure why, but never trust your users not to try.
+				//I believe there should be either it is 0 or above zero and equals but never anything else so we should be good.
+				$table_output = $table_output . $current_row_output; //I like my way that is more reasonable instead of .=
+			}
 		}
+	} //End of if its not current user
+	elseif( $current_user_state == TRUE ) //NOTE: The below is only if it is set for current users
+	{
+		//We need to find out how many times a user is in the table.
+		$number_of_user_rows_query = "SELECT count( user_id ) FROM $table_name_log WHERE id = %d";
+		$number_of_user_rows_query_prepared = $wpdb->prepare( $number_of_user_rows_query, $user_id );
+		$number_of_user_rows = $wpdb->get_var( $number_of_user_rows_query_prepared ); //Ok. I realized that not only prepare() doesn't work it, there is no varialbes needed to sanitize as the table name is actually hard coded.
 
-		$current_row_output = "
-			<tr>
-				<td>$x_for_count</td>
-				<td>$date_data</td>
-				$uid_data_row
-				<td>$display_name_data</td>
-				<td>$point_type_data</td>
-				<td>$amount_data</td>
-				<td>$reason_data</td>
-			</tr>
-				";
+		//The number of log rows will always but correct but its the starting point and end points that will change.
+		for ($x_for_count = $number_of_log_rows; $x_for_count >= 1; $x_for_count = $x_for_count - 1 ) //I'm counting backwards. Also look what I did. Also also, there should never be a 0 id or less than 1
+		{
+			//$date_data = $wpdb->get_var( "SELECT time FROM $table_name_log WHERE id= '$x_for_count'" ); //Straight up going to brute force this un-programatically not via entire row
+			$date_data_query = "SELECT time FROM ". $table_name_log . " WHERE id = %d";
+			$date_data_query_prepared = $wpdb->prepare( $date_data_query, $x_for_count );
+			$date_data = $wpdb->get_var( $date_data_query_prepared );
 
-		//Code inserted to see if a user id was specified. If so, we are creating a table just for that user_id.
-		if( $user_id == 0){
+			//$user_id_data = $wpdb->get_var( "SELECT user_id FROM $table_name_log WHERE id= '$x_for_count'" );
+			$user_id_data_query = "SELECT user_id FROM ". $table_name_log . " WHERE id = %d";
+			$user_id_data_query_prepared = $wpdb->prepare( $user_id_data_query, $x_for_count );
+			$user_id_data = $wpdb->get_var( $user_id_data_query_prepared );
+			$user_id_validated = intval($user_id_data); //I added this extra line to make the return an int as it wasn't being compared correctly as was coming out as a string not a number.
 
-			//Compile into row output.
-			$table_output = $table_output . $current_row_output; //I like my way that is more reasonable instead of .=
+			//$display_name_data = $wpdb->get_var( "SELECT display_name FROM $table_name_users WHERE id= '$user_id_data'" ); //And this is why I didn't call it the entire row by arrow. We are in 4d with multiple tables
+			$display_name_data_query = "SELECT display_name FROM ". $table_name_users . " WHERE id = %d"; //Note: Pulling from WP users table
+			$display_name_data_query_prepared = $wpdb->prepare( $display_name_data_query, $user_id_data );
+			$display_name_data = $wpdb->get_var( $display_name_data_query_prepared );
 
-		} elseif ( $user_id_validated == $user_id AND $user_id > 0) {
+			//$point_id_data = $wpdb->get_var( "SELECT point_id FROM $table_name_log WHERE id= '$x_for_count'" );
+			$point_id_data_query = "SELECT point_id FROM ". $table_name_log . " WHERE id = %d";
+			$point_id_data_query_prepared = $wpdb->prepare( $point_id_data_query, $x_for_count );
+			$point_id_data = $wpdb->get_var( $point_id_data_query_prepared );
 
-			//The idea above is to see if the query for the sql UID pull (validated) $user_id is the same as the query AND is greater than zero.
-			//In theory, you could put a negative number in, but not sure why, but never trust your users not to try.
-			//I believe there should be either it is 0 or above zero and equals but never anything else so we should be good.
-			$table_output = $table_output . $current_row_output; //I like my way that is more reasonable instead of .=
+			//$point_type_data = $wpdb->get_var( "SELECT name FROM $table_name_points WHERE id= '$point_id_data'" );
+			$point_type_data_query = "SELECT name FROM ". $table_name_points . " WHERE id = %d";
+			$point_type_data_query_prepared = $wpdb->prepare( $point_type_data_query, $point_id_data );
+			$point_type_data = $wpdb->get_var( $point_type_data_query_prepared );
 
+			//$amount_data = $wpdb->get_var( "SELECT points_amount FROM $table_name_log WHERE id= '$x_for_count'" );
+	    $amount_data_query = "SELECT points_amount FROM ". $table_name_log . " WHERE id = %d";
+	    $amount_data_query_prepared = $wpdb->prepare( $amount_data_query, $x_for_count );
+	    $amount_data = $wpdb->get_var( $amount_data_query_prepared );
+
+			//$reason_data = $wpdb->get_var( "SELECT reason FROM $table_name_log WHERE id= '$x_for_count'" );
+	    $reason_data_query = "SELECT reason FROM ". $table_name_log . " WHERE id = %d";
+	    $reason_data_query_prepared = $wpdb->prepare( $reason_data_query, $x_for_count );
+	    $reason_data = $wpdb->get_var( $reason_data_query_prepared );
+
+			//If statement to pop in the UID if There
+			//Just popping in to the table if there. Hopefully it doesn't blow up the existing table
+			if( $uid_on == TRUE )
+			{
+				$uid_data_row = "<td>$user_id_data</td>";
+			}
+			else
+			{
+				$uid_data_row = "";
+			}
+
+			$current_row_output = "
+				<tr>
+					<td>$x_for_count</td>
+					<td>$date_data</td>
+					$uid_data_row
+					<td>$display_name_data</td>
+					<td>$point_type_data</td>
+					<td>$amount_data</td>
+					<td>$reason_data</td>
+				</tr>
+					";
+
+			//Code inserted to see if a user id was specified. If so, we are creating a table just for that user_id.
+			if( $user_id == 0)
+			{
+				//Compile into row output.
+				$table_output = $table_output . $current_row_output; //I like my way that is more reasonable instead of .=
+			}
+			elseif ( $user_id_validated == $user_id AND $user_id > 0)
+			{
+				//The idea above is to see if the query for the sql UID pull (validated) $user_id is the same as the query AND is greater than zero.
+				//In theory, you could put a negative number in, but not sure why, but never trust your users not to try.
+				//I believe there should be either it is 0 or above zero and equals but never anything else so we should be good.
+				$table_output = $table_output . $current_row_output; //I like my way that is more reasonable instead of .=
+			}
 		}
-
 	}
 
 	//The page output
