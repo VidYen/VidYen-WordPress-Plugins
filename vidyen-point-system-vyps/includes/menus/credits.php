0956a22e8bd2ac478befc223062dd3d0a9d11fba
Felty <40067341+VidYen@users.noreply.github.com>
2019-01-25 17:29:21 -0500

    updated readme

--- vidyen-point-system-vyps/includes/menus/credits.php
+++ vidyen-point-system-vyps/includes/menus/credits.php
@@ -15,9 +15,9 @@ $vidyen_twitch_plugin_url = 'https://wordpress.org/plugins/vidyen-twitch-player/
 
 echo "
   <h1>Pro Version</h1>
-  <p>There is a pro-plugin valid until 2020 available for purchase on the <a href=\"https://www.vidyen.com/product/vyps-pro-install/\" target=\"_blank\">VidYen</a> store.</p>
-  <p>It will remove the VYPS branding on the monetization pages. NOTE: You can use VYPS to earn credit towards its purchase.</p>
-  <h2>Please vist us for more development work at <a href=\"https://www.vidyen.com/vyps/\" target=\"_blank\">VidYen.com</a></h2>
+  <p>There is a pro plugin valid until 2020 available for purchase on the <a href=\"https://www.vidyen.com/product/vyps-pro-install/\" target=\"_blank\">VidYen</a> store. It will remove the VYPS branding on the monetization pages.</p>
+  <p>NOTE: You can use VYPS to earn credit toward its purchase.</p>
+  <h2>PFor more development work, please check out  <a href=\"https://www.vidyen.com/vyps/\" target=\"_blank\">VidYen.com</a></h2>
   <p>Feel free to ping Felty on the <a href=\"https://discord.gg/6svN5sS\" target=\"_blank\">VidYen Discord</a> if you have any immediate questions.</p>
   <h2>Other VidYen Plugins</h2>";
 
