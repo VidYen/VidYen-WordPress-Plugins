b9509ade561678f197b6eb9bf89b38ca49e234ee
Felty <40067341+VidYen@users.noreply.github.com>
2019-01-29 13:26:06 -0500

    Update wannads-menu.php

--- vidyen-point-system-vyps/includes/menus/wannads-menu.php
+++ vidyen-point-system-vyps/includes/menus/wannads-menu.php
@@ -31,23 +31,23 @@ function vyps_wannads_sub_menu_page()
   echo '<br><br><img src="' . $VYPS_logo_url . '" > ';
 
   //Originally, this was includes to other files but don't need that much de-integration
-  echo '<br><br><h1>VYPS Adscend Media User Tracking API Shortcodes</h1>
+  echo '<br><br><h1>Wannads Tracking API Shortcodes</h1>
 	<p>This plugin needs a <a href="https://www.wannads.com" target="_blank">Wannads Media</a> account to function. The intention is to allow a quick and easy way for you to award user points for Adscend Activity.</p>
 	<p>The intention of this plugin was to make it easier for a WordPress admin to talk with the Adscend API without actually having to program curl calls in PHP manually and automate the recognition of the monetization activity by your end users.</p>
 	<h1>Shortcodes and Syntax</h1>
 	<p><b>[vyps-wannads apikey=(found on Wannads site)]</b></p>
-	<p>This is pretty much it. Just need to grab your Wannads API and post here<p>
+	<p>This is pretty much it. You just need to grab your Wannads API and post it here.<p>
 	<h1>Post Back Support:</h1>
-	<p>As Wannads has no direct redemption API, it was required to create a post back system which is a bit different that everything else up until now.</p>
+	<p>As Wannads has no direct redemption API, it is required to create a post back system that is a bit different from everything else up until now.</p>
 	<p>You can download the plugin off our <a href="https://www.vidyen.com/product/wannads-postback-plugin/" target="_blank">site store</a></p>
-	<p><b>NOTE:</b></p> While you can pay us the $10 with PayPal for the download, you can simply use our referral code and <a href="https://www.vidyen.com/contact/" target="_blank">let us know</a> when you have been approved, and will will manually send you the $10 in credit.</p>
-	<p>Alternatively, you can just use one of the monetization methods to earn the WooCommerce Credit just like a user would on your site.</p>
+	<p><b>NOTE:</b></p> While you can pay us the $10 with PayPal for the download, you can simply use our <a href="https://www.vidyen.com/wannads-signup/" target="_blank">referral code</a> and <a href="https://www.vidyen.com/contact/" target="_blank">let us know</a> when you have been approved, and we will manually send you the $10 in credit.</p>
+	<p>Alternatively, you can just use one of the monetization methods on the VidYen site to earn the WooCommerce Credit, just like a user would on your site.</p>
 	<h1>Post Back Instructions:</h1>
-	<p>1. Create a WordPress stand alone page for the post back.</p>
+	<p>1. Create a WordPress standalone page for the post back.</p>
 	<p>2. Install the plugin off the VidYen store.</p>
-	<p>3. Add the shortcode: <b>[vyps-wannads-postback outputid=(point id) secret=(your Wannads secret key)]</b></p>
+	<p>3. Add the shortcode: <b>[vyps-wannads-postback outputid=(point ID) secret=(your Wannads secret key)]</b></p>
 	<p>4. Make sure no other text or shortcode is in page body. It will cause errors on the Wannads report if you do.</p>
-	<p>5. On Edit Page -> Page Attributes (right hand side on classic editor), set the template to Wannads Post Back Template and update. This will remove all the extra WordPress HTML that would cause the Wannads API to error out on the post back attempt.</p>
-	<p><b>Note:</b> VYPS will do a 1 for 1 and round to the closet whole number based on the Wannads settings on their site. Simply set your Wannads Data to more than 100 per $1.00 USD and set their system to currency round.</p>
+	<p>5. On Edit Page -> Page Attributes (right-hand side on classic editor), set the template to Wannads Post Back Template and update. This will remove all the extra WordPress HTML that would cause the Wannads API to error out on the post back attempt.</p>
+	<p><b>Note:</b> VYPS will do a 1-for-1 and round to the closest whole number based on the Wannads settings on their site. Simply set your Wannads Data to more than 100 per US$1 and set their system to currency round.</p>
  ';
 }
