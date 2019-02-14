60f03195c12d3580fe43b2d113d9df8b2a9df290
Felty <40067341+VidYen@users.noreply.github.com>
2019-02-14 15:00:55 -0500

    login update to check function

--- vidyen-point-system-vyps/includes/shortcodes/vyps-login.php
+++ vidyen-point-system-vyps/includes/shortcodes/vyps-login.php
@@ -31,13 +31,38 @@ function vyps_login_func( $atts )
 
 add_shortcode( 'vyps-lg', 'vyps_login_func');
 
+/*** Function to show image if not logged in ***/
+
+function vyps_login_img_func( $atts )
+{
+	// ATTR for admins to set whatever image
+	$atts = shortcode_atts(
+	array(
+			'url' => '',
+	), $atts, 'vyps-lg-img' );
+
+	$login_url = $atts['url'];
+
+	// Check to see if user is logged in and boot them out of function if they aren't.
+	if ( !is_user_logged_in() AND !empty($login_url) )
+	{
+		$login_img_html_output = '<img src="' . $login_url . '">';
+		return $login_img_html_output;
+	}
+}
+
+/*** Telling WP to use function for shortcode ***/
+
+add_shortcode( 'vyps-lg-img', 'vyps_login_img_func');
+
 
 /*** Profilegrid short code ***/
 
 function vyps_profilegrid_login_func()
 {
 	//Only works if user is not logged in and profilegrid is  installed.... Hrm... just realized they use profiile magic and didn't chagne functions... lol
-	if ( !is_user_logged_in() AND function_exists('vprofile_magic_login_form') )
+	//I think this function will pick up
+	if ( !is_user_logged_in() AND function_exists('profile_magic_registration_form') )
 	{
 		$content = '[PM_Login]';
 
