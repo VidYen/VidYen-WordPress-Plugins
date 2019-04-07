4b6f92621fba43e49a7df62c0d2a6abde483c9d9
Felty <40067341+VidYen@users.noreply.github.com>
2019-04-07 18:45:18 -0400

    Update vidyen-wc-mmo.php

--- vidyen-wc-mmo/vidyen-wc-mmo.php
+++ vidyen-wc-mmo/vidyen-wc-mmo.php
@@ -3,7 +3,7 @@
 Plugin Name:  VidYen MMO Plugin
 Plugin URI:   https://wordpress.org/plugins/vidyen-point-system-vyps/
 Description:  MMO Support Plugin
-Version:      0.0.53
+Version:      0.0.54
 Author:       VidYen, LLC
 Author URI:   https://vidyen.com/
 License:      GPLv2
