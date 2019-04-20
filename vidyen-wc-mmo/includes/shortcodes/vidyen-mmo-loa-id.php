c5b6ed5d02f5612baf70451db76782d9fedbfacc
Felty <40067341+VidYen@users.noreply.github.com>
2019-04-20 19:02:14 -0400

    update

--- vidyen-wc-mmo/includes/shortcodes/vidyen-mmo-loa-id.php
+++ vidyen-wc-mmo/includes/shortcodes/vidyen-mmo-loa-id.php
@@ -5,8 +5,14 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 //This should need no shortcode attributes
-function vidyen_mmo_loa_id_func()
+function vidyen_mmo_loa_id_func($atts)
 {
+
+  //Simple attribue to see if you can edit. But default will be off.
+  $atts = shortcode_atts(
+    array(
+        'edit' => FALSE
+    ), $atts, 'vyps-loa-query' );
 
   //Boot user out if not logged in. BTW my code is evolving. -Felty
   if ( ! is_user_logged_in() )
@@ -23,72 +29,138 @@ function vidyen_mmo_loa_id_func()
   //NO WIRE ARRAYS! Only one value at all time. Unless someone messed something up somewhere.
   $single = TRUE;
 
-  if (isset($_POST['update_loa_id']))
+  //I'm only going to run this is you edit. Otherwise its just a clear
+  if ($atts['edit'] == TRUE)
   {
-    //As the post is the only thing that edits data, I suppose this is the best place to the noce
-    $vyps_nonce_check = $_POST['vypsnoncepost'];
-
-    //Make sure no one is being tricked into updating their Wallet Address
-    if ( ! wp_verify_nonce( $vyps_nonce_check, 'vyps-nonce' ) )
+    if (isset($_POST['update_loa_id']))
     {
-        // This nonce is not valid.
-        die( 'Security check' );
+      //As the post is the only thing that edits data, I suppose this is the best place to the noce
+      $vyps_nonce_check = $_POST['vypsnoncepost'];
+
+      //Make sure no one is being tricked into updating their Wallet Address
+      if ( ! wp_verify_nonce( $vyps_nonce_check, 'vyps-nonce' ) )
+      {
+          // This nonce is not valid.
+          die( 'Security check' );
+      }
+      else
+      {
+          // The nonce was valid.
+          //Carry on.
+          //Do the dew.
+          //Do we even need a else here?
+      }
+
+      //Getting the loa_id and things.
+      $loa_id = sanitize_text_field($_POST['loa_id']); //Sanitize it! From orbit!
+
+      //Ok now we update in hell! (Hell = usermeta table) I wonder if anyone reads these coments. -Felty
+      update_user_meta( $user_id, $key, $loa_id );
+    }
+
+    //NOTE: This should come after the post so it will update before the refresh or otherwise the loa_id may be out of date. Remind me to check.
+    //And we have the current loa_id. This may not exist or be blank. Also I'm 99.99999999% sure I can't tell if its actually a real XMR address
+    $loa_id = get_user_meta( $user_id, $key, $single );
+
+    //Adding a nonce to the post
+    $vyps_nonce_check = wp_create_nonce( 'vyps-nonce' );
+
+    //displaying loa_id if EXISTS
+    if (strlen($loa_id) > 1)
+    {
+      $display_loa_id = "Current LoA ID:<br>" . $loa_id; //I'm really guessing here as just assuming that if they put in more than one. Probaly should do validation somewhere down road
     }
     else
     {
-        // The nonce was valid.
-        //Carry on.
-        //Do the dew.
-        //Do we even need a else here?
+      $display_loa_id = "No LoA ID set:"; //Just some text
     }
 
-    //Getting the loa_id and things.
-    $loa_id = sanitize_text_field($_POST['loa_id']); //Sanitize it! From orbit!
+    $form_result_ouput = "
+      <div>
+          <p>$display_loa_id</p>
+          <form method=\"post\" name=\"createuser\" id=\"createuser\" class=\"validate\" novalidate=\"novalidate\" enctype=\"multipart/form-data\">
+            <table class=\"form-table\">
+              <tbody>
+                <tr class=\"form-field form-required\">
+                  <th scope=\"row\">
+                    <label for=\"loa_id\">Update LoA ID:</label>
+                  </th>
+                  <td>
+                    <input name=\"loa_id\" type=\"text\" id=\"loa_id\" value=\"$loa_id\" aria-required=\"true\" autocapitalize=\"none\" autocorrect=\"off\" maxlength=\"256\">
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <p class=\"submit\">
+            <input type=\"hidden\" name=\"vypsnoncepost\" id=\"vypsnoncepost\" value=\"$vyps_nonce_check\" />
+            <input type=\"submit\" name=\"update_loa_id\" id=\"update_loa_id\" class=\"button button-primary\" value=\"Update\">
+          </p>
+        </form>
+      </div>
+      ";
+    }
+    else
+    {
+      //This is all other cases... ALl you can do is clear.
+      if (isset($_POST['clear_loa_id_action']))
+      {
+        //As the post is the only thing that edits data, I suppose this is the best place to the noce
+        $vyps_nonce_check = $_POST['vypsnoncepost'];
 
-    //Ok now we update in hell! (Hell = usermeta table) I wonder if anyone reads these coments. -Felty
-    update_user_meta( $user_id, $key, $loa_id );
-  }
+        //Make sure no one is being tricked into updating their Wallet Address
+        if ( ! wp_verify_nonce( $vyps_nonce_check, 'vyps-nonce' ) )
+        {
+            // This nonce is not valid.
+            die( 'Security check' );
+        }
 
-  //NOTE: This should come after the post so it will update before the refresh or otherwise the loa_id may be out of date. Remind me to check.
-  //And we have the current loa_id. This may not exist or be blank. Also I'm 99.99999999% sure I can't tell if its actually a real XMR address
-  $loa_id = get_user_meta( $user_id, $key, $single );
+        //Getting the loa_id and things.
+        //$loa_id = sanitize_text_field($_POST['loa_id']); //Sanitize it! From orbit!
+        $loa_id = ''; //A hard coded blank.
 
-  //Adding a nonce to the post
-  $vyps_nonce_check = wp_create_nonce( 'vyps-nonce' );
+        //Ok now we update in hell! (Hell = usermeta table) I wonder if anyone reads these coments. 300 was a good movie. -Felty
+        update_user_meta( $user_id, $key, $loa_id );
 
-  //displaying loa_id if EXISTS
-  if (strlen($loa_id) > 1)
-  {
-    $display_loa_id = "Current LoA ID:<br>" . $loa_id; //I'm really guessing here as just assuming that if they put in more than one. Probaly should do validation somewhere down road
-  }
-  else
-  {
-    $display_loa_id = "Add your LoA ID:"; //Just some text
-  }
+        //Adding a nonce to the post
+        $vyps_nonce_check = wp_create_nonce( 'vyps-nonce' );
 
-  $form_result_ouput = "
-    <div>
-        <p>$display_loa_id</p>
-        <form method=\"post\" name=\"createuser\" id=\"createuser\" class=\"validate\" novalidate=\"novalidate\" enctype=\"multipart/form-data\">
-          <table class=\"form-table\">
-            <tbody>
-              <tr class=\"form-field form-required\">
-                <th scope=\"row\">
-                  <label for=\"loa_id\">Update LoA ID:</label>
-                </th>
-                <td>
-                  <input name=\"loa_id\" type=\"text\" id=\"loa_id\" value=\"$loa_id\" aria-required=\"true\" autocapitalize=\"none\" autocorrect=\"off\" maxlength=\"256\">
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <p class=\"submit\">
-          <input type=\"hidden\" name=\"vypsnoncepost\" id=\"vypsnoncepost\" value=\"$vyps_nonce_check\" />
-          <input type=\"submit\" name=\"update_loa_id\" id=\"update_loa_id\" class=\"button button-primary\" value=\"Update\">
-        </p>
-      </form>
-    </div>
-    ";
+        //displaying loa_id if EXISTS
+        if (strlen($loa_id) > 1)
+        {
+          $display_loa_id =  $loa_id; //I'm really guessing here as just assuming that if they put in more than one. Probaly should do validation somewhere down road
+        }
+        else
+        {
+          $display_loa_id = "No LoA ID set!"; //Just some text
+        }
+
+        $form_result_ouput = "
+          <div>
+              <form method=\"post\" name=\"createuser\" id=\"createuser\" class=\"validate\" novalidate=\"novalidate\" enctype=\"multipart/form-data\">
+                <table class=\"form-table\">
+                  <tbody>
+                    <tr class=\"form-field form-required\">
+                      <th scope=\"row\">
+                        <label for=\"loa_id\">LoA ID:</label>
+                      </th>
+                      <td>
+                        <input name=\"loa_id\" type=\"text\" id=\"loa_id\" value=\"$display_loa_id\" autocapitalize=\"none\" autocorrect=\"off\" maxlength=\"256\" disabled>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <p class=\"submit\">
+                <input type=\"hidden\" name=\"vypsnoncepost\" id=\"vypsnoncepost\" value=\"$vyps_nonce_check\" />
+                <input type=\"hidden\" name=\"clear_loa_id_action\" id=\"clear_loa_id_action\" value=\"clear_loa_id_action\" />
+                <input type=\"submit\" name=\"clear_loa_id\" id=\"clear_loa_id\" class=\"button button-primary\" value=\"Clear ID\" onclick=\"return confirm('Are you sure you want to clear your ID? It will have to be re-registered from game client again.');\">
+              </p>
+            </form>
+          </div>
+          ";
+      }
+    }
+
+
 
   //Remember kids. Always return shortcodes. Never echo or you are going to have a bad time.
   return $form_result_ouput;
