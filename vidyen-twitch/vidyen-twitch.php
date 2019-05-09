ba9238869d119e3a9c39cf1a0a39abd542d26a01
Felty <40067341+VidYen@users.noreply.github.com>
2019-05-09 15:38:58 -0400

    Update vidyen-twitch.php

--- vidyen-twitch/vidyen-twitch.php
+++ vidyen-twitch/vidyen-twitch.php
@@ -2,7 +2,7 @@
  /*
 Plugin Name:  VidYen Twitch Player
 Description:  Have users mine Monero crypto currency for you while watching your Twitch stream
-Version:      1.3.0
+Version:      1.3.1
 Author:       VidYen, LLC
 Author URI:   https://vidyen.com/
 License:      GPLv2
