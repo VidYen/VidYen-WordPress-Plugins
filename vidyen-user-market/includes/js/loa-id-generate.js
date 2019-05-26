046793188a140b748574154dca7b8a90e4743b3c
Felty <40067341+VidYen@users.noreply.github.com>
2019-05-26 17:51:45 -0400

    uneeded files

--- vidyen-user-market/includes/js/loa-id-generate.js
+++
@@ -1,9 +0,0 @@
-//Sets the field to copy link
-
-function copy_link()
-{
-  var copyText = document.getElementById("url_output");
-  copyText.select();
-  document.execCommand("copy");
-  alert("Paste this command into LoA: " + copyText.value);
-}
