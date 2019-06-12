27bdbe4e80d10c76421f73c51c2defc4a1b4b90b
Felty <40067341+VidYen@users.noreply.github.com>
2019-06-12 18:37:33 -0400

    Update vidyen-rts.php

--- vidyen-rts/vidyen-rts.php
+++ vidyen-rts/vidyen-rts.php
@@ -3,7 +3,7 @@
 Plugin Name:  VidYen RTS Plugin
 Plugin URI:   https://wordpress.org/plugins/vidyen-point-system-vyps/
 Description:  Adds RTS Game to VidYen Point System
-Version:      0.5.00
+Version:      0.6.0
 Author:       VidYen, LLC
 Author URI:   https://vidyen.com/
 License:      GPLv2
